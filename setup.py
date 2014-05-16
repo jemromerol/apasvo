ed2df0987bfd6007c7d0c84392a14c34b24b07f8
Jose Emilio Romero <jemromerol@gmail.com>
2014-05-16 18:39:00 +0200

    Add MANIFEST.in file Add url metadata to setup.py

--- setup.py
+++ setup.py
@@ -47,10 +47,10 @@ with open('requirements.txt') as f:
 
 setup(name="APASVO",
       version=get_version_number(),
-      description="A set of tools to perform seismic event detection and picking",
+      description="A graphical tool to perform event detection/picking in seismic traces.",
       author="Jose Emilio Romero Lopez",
       author_email="jemromerol@gmail.com",
-      url="",
+      url="https://github.com/jemromerol/apasvo",
       license="GPL",
       scripts=["bin/apasvo-detector.py", "bin/apasvo-generator.py", "bin/apasvo-gui.py"],
       install_requires=requirements,
